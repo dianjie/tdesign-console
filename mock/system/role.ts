70395be0260785944308f86c02dfcce677e3d7a6
xiaoyu <dianjie1022@gmail.com>
2022-11-14 11:19:50 +0800

    feat: 增加角色管理mock数据

--- mock/system/role.ts
+++ mock/system/role.ts
@@ -1,5 +1,87 @@
 import type { MockMethod } from 'vite-plugin-mock';
 
+const list = [
+  {
+    id: '1123598816738675201',
+    tenantId: '000000',
+    parentId: '0',
+    roleName: '超级管理员',
+    sort: 1,
+    roleAlias: 'administrator',
+    isDeleted: 0,
+    parentName: '顶级',
+    hasChildren: false,
+  },
+  {
+    id: '1123598816738675202',
+    tenantId: '000000',
+    parentId: '0',
+    roleName: '用户',
+    sort: 2,
+    roleAlias: 'user',
+    isDeleted: 0,
+    children: [
+      {
+        id: '1123598816738675203',
+        tenantId: '000000',
+        parentId: '1123598816738675202',
+        roleName: '人事',
+        sort: 1,
+        roleAlias: 'hr',
+        isDeleted: 0,
+        parentName: '用户',
+        hasChildren: false,
+      },
+      {
+        id: '1123598816738675204',
+        tenantId: '000000',
+        parentId: '1123598816738675202',
+        roleName: '经理',
+        sort: 2,
+        roleAlias: 'manager',
+        isDeleted: 0,
+        parentName: '用户',
+        hasChildren: false,
+      },
+      {
+        id: '1123598816738675205',
+        tenantId: '000000',
+        parentId: '1123598816738675202',
+        roleName: '老板',
+        sort: 3,
+        roleAlias: 'boss',
+        isDeleted: 0,
+        parentName: '用户',
+        hasChildren: false,
+      },
+    ],
+    parentName: '顶级',
+    hasChildren: false,
+  },
+  {
+    id: '1226473225100234753',
+    tenantId: '763196',
+    parentId: '0',
+    roleName: '管理员',
+    sort: 2,
+    roleAlias: 'admin',
+    isDeleted: 0,
+    parentName: '顶级',
+    hasChildren: false,
+  },
+  {
+    id: '1226473261875892226',
+    tenantId: '648897',
+    parentId: '0',
+    roleName: '管理员',
+    sort: 2,
+    roleAlias: 'admin',
+    isDeleted: 0,
+    parentName: '顶级',
+    hasChildren: false,
+  },
+];
+
 export default [
   {
     url: '/api/blade-system/role/tree',
@@ -52,6 +134,31 @@ export default [
             value: '1123598816738675202',
           },
         ],
+        msg: '操作成功',
+      };
+    },
+  },
+  {
+    url: '/api/blade-system/role/list',
+    method: 'get',
+    response: () => {
+      return {
+        code: 200,
+        success: true,
+        data: list,
+        msg: '操作成功',
+      };
+    },
+  },
+  {
+    url: '/api/blade-system/role/detail',
+    method: 'get',
+    response: ({ query }) => {
+      const { id } = query;
+      return {
+        code: 200,
+        success: true,
+        data: list.find((item) => item.id === id) || {},
         msg: '操作成功',
       };
     },
